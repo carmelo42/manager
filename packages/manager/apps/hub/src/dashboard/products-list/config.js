512d6dec5cdc3758315b73c57e43c74626b1b863
Marie JONES <marie.jones@corp.ovh.com>
2020-07-23 17:02:03 +0200

    feat(products-list): allow to use product configuration
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/apps/hub/src/dashboard/products-list/config.js
+++ packages/manager/apps/hub/src/dashboard/products-list/config.js
@@ -1,54 +1,45 @@
-import { find, get, head, isEmpty, map, startCase } from 'lodash-es';
+import {
+  camelCase,
+  find,
+  get,
+  head,
+  isEmpty,
+  map,
+  pickBy,
+  startCase,
+} from 'lodash-es';
 
 import { ListLayoutHelper } from '@ovh-ux/manager-ng-layout-helpers';
+import columnsConfiguration from '@ovh-ux/manager-product-listing-configuration';
 import { DEFAULT_NUMBER_OF_COLUMNS } from './constants';
 
-const matchingTypes = {
-  boolean: 'boolean',
-  enum: 'string',
-  long: 'number',
-  string: 'string',
+const mapApiProperties = (properties) => {
+  const displayedProperties = pickBy(
+    properties,
+    (property) => ListLayoutHelper.matchingTypes[property.type],
+  );
+  return map(displayedProperties, (value, name) => ({
+    label: name,
+    property: name,
+  }));
 };
 
-const getSorting = ({ sort, sortOrder }, property) =>
-  sort === property ? sortOrder.toLowerCase() : true;
-
-const mapApiProperties = (properties, sorting) =>
-  map(properties, (value, name) => ({
-    title: name,
-    property: name,
-    type: /Enum/.test(value.type)
-      ? matchingTypes.enum
-      : matchingTypes[value.type],
-    searchable: true,
-    filterable: true,
-    sortable: getSorting(sorting, name),
+const getDefaultConfiguration = (dataModel, propertyId, displayedColumns) => {
+  const columns = mapApiProperties(dataModel.properties).filter(
+    ({ label }) => label !== propertyId,
+  );
+  columns.unshift({
+    label: propertyId,
+    property: propertyId,
+    serviceLink: true,
+  });
+  return columns.map((column, index) => ({
+    ...column,
+    label: startCase(column.label),
+    hidden: isEmpty(displayedColumns)
+      ? index > DEFAULT_NUMBER_OF_COLUMNS
+      : !displayedColumns.includes(column.label),
   }));
-
-const getFirstColumnTemplate = (propertyId, sorting) => ({
-  title: propertyId,
-  property: propertyId,
-  template: `
-      <a
-        data-ng-href="{{ $row.managerLink }}"
-        data-ng-bind="$row.${propertyId}"
-        data-track-on="click"
-        data-track-name="hub::product-listing::go-to-service"></a>
-    `,
-  searchable: true,
-  filterable: true,
-  sortable: getSorting(sorting, propertyId),
-});
-
-export const urlQueryParams = `columns&${ListLayoutHelper.urlQueryParams}`;
-
-export const params = {
-  columns: {
-    squash: true,
-    value: '[]',
-    dynamic: true,
-  },
-  ...ListLayoutHelper.stateParams,
 };
 
 export const component = 'managerListLayout';
@@ -58,6 +49,7 @@ export const resolves = {
     get(services, `data.data.${productType}.data`),
   resourcePath: /* @ngInject */ (products) => get(head(products), 'route.path'),
   propertyId: /* @ngInject */ (products) => get(head(products), 'propertyId'),
+
   dataModel: /* @ngInject */ (resourcePath, schema) => {
     const model = get(
       find(get(find(schema.apis, { path: resourcePath }), 'operations'), {
@@ -67,38 +59,35 @@ export const resolves = {
     );
     return schema.models[model];
   },
+
+  serviceNameTracker: () => 'hub::product-listing::go-to-service',
+
+  columnConfig: /* @ngInject */ (
+    $q,
+    dataModel,
+    displayedColumns,
+    productType,
+    propertyId,
+  ) =>
+    columnsConfiguration[camelCase(productType)]
+      ? columnsConfiguration[camelCase(productType)].getConfig()
+      : $q.resolve({
+          data: getDefaultConfiguration(
+            dataModel,
+            propertyId,
+            displayedColumns,
+          ),
+        }),
+
   displayedColumns: /* @ngInject */ ($transition$) =>
     JSON.parse($transition$.params().columns),
-  loadRow: /* @ngInject */ (products, propertyId) => (service) => ({
-    ...service,
-    managerLink: get(
+
+  getServiceNameLink: /* @ngInject */ (products, propertyId) => (service) =>
+    get(
       products.find(({ resource }) => resource.name === service[propertyId]),
       'url',
     ),
-  }),
-  columns: /* @ngInject */ (
-    dataModel,
-    displayedColumns,
-    propertyId,
-    sort,
-    sortOrder,
-  ) => {
-    const columns = mapApiProperties(dataModel.properties, {
-      sort,
-      sortOrder,
-    }).filter(({ title, type }) => type && title !== propertyId);
-    columns.unshift(getFirstColumnTemplate(propertyId, { sort, sortOrder }));
-    return columns.map((column, index) => ({
-      ...column,
-      title: startCase(column.title),
-      hidden: isEmpty(displayedColumns)
-        ? index > DEFAULT_NUMBER_OF_COLUMNS
-        : !displayedColumns.includes(column.title),
-    }));
-  },
-  hideBreadcrumb: () => false,
-  breadcrumb: /* @ngInject */ ($translate, productType) =>
-    $translate.instant(`manager_hub_products_${productType}`),
+
   id: /* @ngInject */ (productType) => productType,
   defaultFilterColumn: /* @ngInject */ (propertyId) => propertyId,
   header: /* @ngInject */ ($translate, productType) =>
@@ -115,11 +104,13 @@ export const resolves = {
     ></ovh-manager-hub-carousel>`;
   },
   customizableColumns: () => true,
+
+  hideBreadcrumb: () => false,
+  breadcrumb: /* @ngInject */ ($translate, productType) =>
+    $translate.instant(`manager_hub_products_${productType}`),
 };
 
 export default {
-  urlQueryParams,
-  params,
   component,
   resolves,
 };
