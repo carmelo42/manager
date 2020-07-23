0d19ca750c10a43755dd4d44e0456d292254f5bf
Marie JONES <marie.jones@corp.ovh.com>
2020-07-23 17:16:53 +0200

    feat(overthebox): add product listing configuration
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

--- packages/manager/modules/product-configuration/src/index.js
+++ packages/manager/modules/product-configuration/src/index.js
@@ -1,12 +1,14 @@
 import './i18n';
 import freefax from './freefax';
 import packXdsl from './pack-xdsl';
+import overTheBox from './overthebox';
 import sms from './sms';
 import telephony from './telephony';
 import telephonyServices from './telephony/services';
 
 export {
   freefax,
+  overTheBox,
   packXdsl,
   sms,
   telephony,
@@ -15,6 +17,7 @@ export {
 
 export default {
   freefax,
+  overTheBox,
   packXdsl,
   sms,
   telephony,
